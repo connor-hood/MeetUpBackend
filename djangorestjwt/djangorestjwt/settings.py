f7af524724d9b8725a98d664f1dd194cc69b224a
connor-hood <81389013+connor-hood@users.noreply.github.com>
2021-08-01 18:33:59 -0500

    push to hopefully fix migration issue

--- djangorestjwt/djangorestjwt/settings.py
+++ djangorestjwt/djangorestjwt/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'meetUp.apps.MeetupConfig',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'meetUp.User'
