c1d665b68c8240ec3c70e6d359243998b00df8c6
connor-hood <81389013+connor-hood@users.noreply.github.com>
2021-08-09 21:22:33 -0500

    restarted backend, following tutorial

--- djangorestjwt/meetUp/urls.py
+++
@@ -1,7 +0,0 @@
-from .views import RegisterView, LoginView
-from django.urls import path, include
-
-urlpatterns = [
-    path('register/', RegisterView.as_view()),
-    path('login/', LoginView.as_view()),
-]
---
+++ jwtbackend/reviews/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Category, Company, Comment, Product, ProductSite, ProductSize
+
+# Register your models here.
+admin.site.register(Category)
+admin.site.register(Company)
+admin.site.register(Comment)
+admin.site.register(Product)
+admin.site.register(ProductSite)
+admin.site.register(ProductSize)
+
+# this changes the yellow text in admin
+admin.site.site_header = "Product Review Admin"
+
+# if we want to remove group model from Admin site:
+# import Group from django.contrib.auth.models
+# admin.site.unregister(Group)
